99e76bcf750b519dcd8afe13f57daae7d4106cd3
Joko15000 <153940286+Joko15000@users.noreply.github.com>
2025-11-24 13:26:00 +0700

    Update MainContent.tsx

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -317,13 +317,9 @@ const MainContent: React.FC<MainContentProps> = ({ partnerName, partnerAge }) =>
           <HugIcon />
           Hug Virtual
         </button>
-        <button
-        
-          className="mt-8 flex items-center gap-3 mx-auto px-8 py-4 bg-sky-400 text-white font-bold rounded-full shadow-lg hover:bg-sky-500 transform hover:scale-105 transition-all duration-300"
-        >
+        <button className="mt-8 flex items-center gap-3 mx-auto px-8 py-4 bg-sky-400 text-white font-bold rounded-full shadow-lg hover:bg-sky-500 transform hover:scale-105 transition-all duration-300">
           ✉︎ 
           <a href="https://wa.me/6281410149687?text=Udah%20aku%20baca%20sayang%20Loveyou"></a>
-
         </button>
       </section>
     </main>
